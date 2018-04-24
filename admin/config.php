c480f502de0dc816c1b59c7de7a1ea6152c202fd
pizepei <848733913@qq.com>
2018-04-24 16:41:49 +0800

    补充缺失的模块路由

--- admin/config.php
+++ admin/config.php
@@ -101,6 +101,7 @@ return [
         'route'.DS.'menu',
         'route'.DS.'ssr',
         'route'.DS.'user',
+        'route'.DS.'authority',
     ],
     // 是否强制使用路由
     'url_route_must'         => false,
---
+++ admin/route/authority.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @Author: pizepei
+ * @Date:   2018-04-24 15:52:58
+ * @Last Modified by:   pizepei
+ * @Last Modified time: 2018-04-24 16:41:35
+ */
+// 权限模块   路由规则
+use think\Route;
